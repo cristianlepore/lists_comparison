551df0e63a4ed5c5179cdd4755dcb95004dcf1c1
Cristian Lepore <cristianlepore24@gmail.com>
2023-10-24 09:31:13 +0200

    Update List_similarity.py

--- List_similarity.py
+++ List_similarity.py
@@ -1,5 +1,10 @@
 import os, re, csv
 
+# Set your parameters:
+similarity_grade = 0.5
+file_name_orbis = "orbis.txt"
+file_name_pitchbook = "pitchbook.txt"
+
 type_society = ["SRL", "SPA", "SNC", "SAPA", "SAS", "SCRL", "SCPA", "SS", "SNCA", "SRLS", "SPAA", "SPAR", "SPAP", "SPAPA", "SRC", "LTD", "INC", "CO", "CORP", "LLC", "PLC"]
 
 def get_absolute_path(file_name):
@@ -15,7 +20,12 @@ def process_line(line):
     line = line.upper()
     line = ' '.join(re.sub(r'\.', '', word) for word in re.findall(r'[a-zA-Z0-9.]+', line))
     line = re.sub(r'\s+', ' ', line)
+
+    for society_type in type_society:
+        line = re.sub(rf'\b{society_type}\b', f'\n{society_type}\n', line)
+    
     line = re.sub('|'.join(map(re.escape, type_society)), substitute_type_society, line)
+
     return line.strip()
 
 def read_file(file_name):
@@ -26,9 +36,11 @@ def read_file(file_name):
     if os.path.exists(file_path):
         with open(file_path, "r") as file:
             for line in file:
-                line = process_line(line)
-                if line:
-                    processed_lines.add(line)
+                lines = process_line(line)
+                for processed_line in lines.split('\n'):
+                    processed_line = processed_line.strip()
+                    if processed_line:
+                        processed_lines.add(processed_line)
 
     return sorted(list(processed_lines))
 
@@ -63,7 +75,8 @@ def words_means(str):
 def ngram_similarity(element1, element2):
 
     # Definisco il parametro n
-    n = int(words_means(element1))
+    #n = int(words_means(element1))
+    n = 3
 
     ngrams_element1 = generate_ngrams(element1, n)
     ngrams_element2 = generate_ngrams(element2, n)
@@ -79,35 +92,55 @@ def ngram_similarity(element1, element2):
 def find_similarity(list1, list2):
     # Trova elementi simili tra due liste
     similar = []
+    # Lista per elementi di list1 che non passano il test
+    different_in_list1 = []
+    remaining_list2 = list2[:]
+
     for element1 in list1:
+        found_similar = False
         for element2 in list2:
             similarity = ngram_similarity(element1, element2)
-            if similarity >= 0.55:
+            if similarity >= similarity_grade:
                 similar.append((element1+" || "+element2))
-    return similar
+                found_similar = True
+                # Verifica se l'elemento esiste ancora in remaining_list2
+                if element2 in remaining_list2: 
+                    remaining_list2.remove(element2)
+
+        if not found_similar:
+            different_in_list1.append(element1)
+
+    different_in_list2 = remaining_list2  # Gli elementi rimanenti in list2 sono diversi
+
+    return similar, different_in_list1, different_in_list2
+
+def write_to_file(name_file, elements):
+    # Apre il file in modalità scrittura
+    with open(name_file, mode='w', newline='') as file_csv:
+        writer = csv.writer(file_csv)
+        
+        # Scrive i dati nella tabella CSV
+        for element in elements:
+            writer.writerow([element])
 
 def main():
-    file_name_orbis = "OrbisCSV.CSV"
-    file_name_pitchbook = "PitchbookCSV.CSV"
-
     list_orbis = read_file(file_name_orbis)
     list_pitchbook = read_file(file_name_pitchbook)
 
-    similar_elements = find_similarity(list_orbis, list_pitchbook)
+    results = find_similarity(list_orbis, list_pitchbook)
 
     # Specifica il nome del file CSV in cui desideri scrivere i dati
-    nome_file_csv = "Similar_companies.csv"
+    file_similarity = "Orbis_vs_Pitchbook.csv"
+    file_excluded_orbis = "Orbis_excluded.csv"
+    file_excluded_pitchbook = "Pitchbook_excluded.csv"
 
-    # Apre il file in modalità scrittura
-    with open(nome_file_csv, mode='w', newline='') as file_csv:
-        writer = csv.writer(file_csv)
+    write_to_file(file_similarity, results[0])
+    write_to_file(file_excluded_orbis, results[1])
+    write_to_file(file_excluded_pitchbook, results[2])
 
-        # Scrive i dati nella tabella CSV
-        for element in similar_elements:
-            writer.writerow([element])
-
-    print(f"Numero di aziende simili: {len(similar_elements)}")
-    print(f"I dati sono stati scritti nel file '{nome_file_csv}'.")
-
+    print(f"Record simili: {len(results[0])}")    
+    print(f"Esclusi Orbis: {len(results[1])}")
+    print(f"Esclusi Pitchbook: {len(results[2])}")
+    
 if __name__ == "__main__":
     main()
